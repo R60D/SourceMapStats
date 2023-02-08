Refactoring code and fixing bugs. New algo for calculating average playercount.

read.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+
 from math import pi
 import numpy as np
 import csv
@@ -7,36 +8,64 @@ from datetime import datetime, timedelta
 import os
 from R6LIB import dictmerger,dictmax,dictlimx,arrayrectifier,weakfiller
 import Parameters as p
+import argparse
 
+#Arguments
+parser = argparse.ArgumentParser(description="SourceMapStats reader. Defaults in parameter.py",formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("-f", "--filename", type=str, help="Filename for reading",default=p.Filename)
+parser.add_argument("-fp", "--filenamepng", type=str, help="Output png of bar graphs",default=p.Filenamepng)
+parser.add_argument("-mts", "--mapstoshow", type=int, help="How many maps to show",default=p.MapsToShow)
+parser.add_argument("-sd", "--startdate", type=str, help="Specify start date of data",default=p.Start_Date)
+parser.add_argument("-ed", "--enddate", type=str, help="Specify end date of data",default=p.End_Date)
+parser.add_argument("-only", "--onlymaps", type=list, help="word whitelist",default=p.OnlyMapsContaining)
+parser.add_argument("-fl", "--filter", type=list, help="Words to remove from maps",default=p.wordfilter)
+parser.add_argument("-vf", "--versionfilter", type=list, help="Versions to remove",default=p.versionfilter)
+parser.add_argument("-pc", "--percision", type=int, help="Decimal place for figure",default=p.Percision)
+parser.add_argument("-lb", "--labeltransparency", type=int, help="Opacity of name label",default=p.LabelTransparency)
+parser.add_argument("-od", "--outputdimensions", type=tuple, help="Output dimensions of png",default=p.OutputDimensions)
+parser.add_argument("-oth", "--othermapcolor", type=tuple, help="Other maps color",default=p.ColorForOtherMaps)
+parser.add_argument("-avg", "--averagedays", type=int, help="How many days each bar represents",default=p.AverageDays)
+parser.add_argument("-ci", "--colorintensity", type=int, help="how much color changes per map",default=p.ColorIntensity)
+parser.add_argument("-ad", "--axisdates", type=int, help="How many dates to display on the x axis",default=p.XaxisDates)
+args = parser.parse_args()
+config = vars(args)
 #The reader assumes that the CSV input dates are in this format.
 ReaderTimeFormat = '%Y-%m-%d-%H:%M:%S'
-#Makes a nice rainbow
-
-
-#An estimation of Daily Average count for a given gamemode in a given game. Average of Daily Averages using unique ips.
-#Takes into account writer pace and server count.
-def DailyAverage(dailydata):
-    totalsum = 0
-    for day in dailydata:
-        dailydict = {}
-        for datapoint in day:
-            dataip = str(datapoint[0])+":"+str(datapoint[1])
-            playercount = int(datapoint[3])
-            if(dataip in dailydict.keys()):
-                currplayer = dailydict[dataip]
-                dailydict.update({dataip:playercount+currplayer})
+def IndexAverage(Rawdata):
+    CurrentScanIndex = 0
+    CurrentScanIndexlist = []
+    ScanAverage = []
+    for scan in Rawdata:
+        try:
+            curint = int(scan[6])
+            if(CurrentScanIndex == curint):
+                CurrentScanIndexlist.append(curint)
             else:
-                dailydict.update({dataip:playercount})
-        RepaterCount = len(day)/len(dailydict.keys())
+                sumR = sum(CurrentScanIndexlist)
+                if(sumR != 0):
+                    ScanAverage.append(sum(CurrentScanIndexlist))
+                CurrentScanIndexlist = []
+                CurrentScanIndex = curint
+                CurrentScanIndexlist.append(curint)
+        except:
+            None
 
+            if(len(ScanAverage) != 0):
+                return round(sum(ScanAverage)/len(ScanAverage),config['percision'])
+            else:
+                return 0
         
-        #add the average playercount for the day into the total sum
-        totalsum+=sum(dailydict.values())/(RepaterCount)
-    #take the average of the totalsum relative to days
-    return round(totalsum/len(dailydata),p.Percision)
 
-
-
+    
+def RawData():
+    iplist = []
+    dirname = os.path.dirname(os.path.realpath(__file__))
+    rawfilename = os.path.join(dirname,config["filename"])
+    with open(rawfilename,"r") as filedata:
+        csvreader = csv.reader(filedata)
+        for row in csvreader:
+            iplist.append(row)
+    return iplist
 
 def ColorGen():
     global previouscolor
@@ -44,34 +73,34 @@ def ColorGen():
     previouscolor += 2*pi/(mapcount)
 
 
-    amplitude   = np.sin(previouscolor*p.ColorIntensity)
-    amplitude2   = np.sin((previouscolor+2*pi/3)*p.ColorIntensity)
-    amplitude3   = np.sin((previouscolor+4*pi/3)*p.ColorIntensity)
+    amplitude   = np.sin(previouscolor*config['colorintensity'])
+    amplitude2   = np.sin((previouscolor+2*pi/3)*config['colorintensity'])
+    amplitude3   = np.sin((previouscolor+4*pi/3)*config['colorintensity'])
     if( not force):
         col = ((amplitude+1)/2, (amplitude2+1)/2, (amplitude3+1)/2)
-    elif(p.ColorForOtherMaps == None):
+    elif(config['othermapcolor'] == None):
         col = ((amplitude+1)/2, (amplitude2+1)/2, (amplitude3+1)/2)
     else:
-        col = p.ColorForOtherMaps
+        col = config['othermapcolor']
     return col
 #Using MatPlotLib this will draw the actual visual side of the output.
 def plotdraw(_X,_label):
+
     global previousmap
     global percentageSanity
     col = ColorGen()
 
     if(force):
-        labelpercn = round(100-percentageSanity,p.Percision)
+        labelpercn = round(100-percentageSanity,config['percision'])
         Labels.append((f"{_label} : {labelpercn} %",labelpercn))
         previousmap = previousmap.tolist()
         Handles.append(ax.bar(np.arange(len(_Y)),_Y, color=col,bottom=previousmap[0:len(_Y)],width=np.array(YMAX)/max(YMAX)))
         previousmap = previousmap+np.array(_Y)
         percentageSanity += labelpercn
-        #print(percentageSanity)
 
 
     elif(type(previousmap) is not type(None)):
-        labelpercn = round(100*(sum(_Y)/sum(YMAX)),p.Percision)
+        labelpercn = round(100*(sum(_Y)/sum(YMAX)),config['percision'])
         Labels.append((f"{_label} : {labelpercn} %",labelpercn))
         previousmap = previousmap.tolist()
         Handles.append(ax.bar(np.arange(len(_Y)),_Y/YMAX, color=col,bottom=previousmap[0:len(_Y)],width=np.array(YMAX)/max(YMAX)))
@@ -79,7 +108,7 @@ def plotdraw(_X,_label):
         percentageSanity += labelpercn
 
     else:
-        labelpercn = round(100*(sum(_Y)/sum(YMAX)),p.Percision)
+        labelpercn = round(100*(sum(_Y)/sum(YMAX)),config['percision'])
         Labels.append((f"{_label} : {labelpercn} %",labelpercn))
         Handles.append(ax.bar(np.arange(len(_Y)),_Y/YMAX, color=col,width=np.array(YMAX)/max(YMAX)))
         previousmap = np.array(_Y/YMAX)
@@ -93,41 +122,35 @@ def plotdraw(_X,_label):
     ax.set_xticks(np.linspace(ax.get_xlim()[0],ax.get_xlim()[1],len(xtimes)),xtimes)
 #"Chunks" dates together and averages them into bars.
 def timechunker(AverageDays):
-    startdateobject = datetime.strptime(p.Start_Date, '%Y-%m-%d')
-    enddateobject = datetime.strptime(p.End_Date, '%Y-%m-%d')
+    startdateobject = datetime.strptime(config["startdate"], '%Y-%m-%d')
+    enddateobject = datetime.strptime(config["enddate"], '%Y-%m-%d')
     chunkedarray = []
     chunkedarray_index = 0
     chunktime = timedelta(AverageDays,0,0,0,0,0,0)#Interval means days here
     initialtime = None
-
-    dirname = os.path.dirname(os.path.realpath(__file__))
-    rawfilename = os.path.join(dirname,p.Filename)
-    with open(rawfilename,"r") as serverdata:
-        rawrawstat = csv.reader(serverdata)
-        rawstat = []
-        for row in rawrawstat:
-            datetime_object = datetime.strptime(row[4], ReaderTimeFormat)
-            if(datetime_object > startdateobject and datetime_object < enddateobject):
-                rawstat.append(row)
+    rawstat = []
+    for row in CSVDATA:
+        datetime_object = datetime.strptime(row[4], ReaderTimeFormat)
+        if(datetime_object > startdateobject and datetime_object < enddateobject):
+            rawstat.append(row)
         
-        
-        for row in rawstat:
+    for row in rawstat:
 
-            if(initialtime == None):
-                initialtime = datetime.strptime(row[4], ReaderTimeFormat) #get the first row time
+        if(initialtime == None):
+            initialtime = datetime.strptime(row[4], ReaderTimeFormat) #get the first row time
 
-            datetime_object = datetime.strptime(row[4], ReaderTimeFormat)
-            if(len(chunkedarray)-1 == chunkedarray_index):
-                chunkedarray[chunkedarray_index].append(row)
-            else:
-                chunkedarray.append([])
-                chunkedarray[chunkedarray_index].append(row)
+        datetime_object = datetime.strptime(row[4], ReaderTimeFormat)
+        if(len(chunkedarray)-1 == chunkedarray_index):
+            chunkedarray[chunkedarray_index].append(row)
+        else:
+            chunkedarray.append([])
+            chunkedarray[chunkedarray_index].append(row)
 
-            if(datetime_object > initialtime+chunktime):
-                chunkedarray_index += 1
-                while(datetime_object > initialtime+chunktime):
-                    chunktime += timedelta(AverageDays,0,0,0,0,0,0)
-                    #print(chunktime)
+        if(datetime_object > initialtime+chunktime):
+            chunkedarray_index += 1
+            while(datetime_object > initialtime+chunktime):
+                chunktime += timedelta(AverageDays,0,0,0,0,0,0)
+                #print(chunktime)
 
     return chunkedarray
 #Wrapper around SuffixFilter
@@ -156,7 +179,7 @@ def SuffixFilter(x):
     z = x[y:]
     e = z.split("_")
 
-    for n in p.wordfilter:
+    for n in config['filter']:
         for idx, n2 in enumerate(e):
             if(n == n2 or n == n2[::-1]):
                 e.pop(idx)
@@ -172,6 +195,7 @@ def SuffixFilter(x):
     return newname
 #Does the heavy lifting and makes sure that data is correct
 def plotter():
+    print("START")
     global fig
     global ax
     global previousmap
@@ -183,8 +207,9 @@ def plotter():
     global YMAX
     global _Y
     global percentageSanity
-
-    fig,ax = plt.subplots(figsize=p.OutputDimensions)
+    global CSVDATA
+    CSVDATA = RawData()
+    fig,ax = plt.subplots(figsize=config['outputdimensions'])
 
     percentageSanity = 0
     Labels = []
@@ -194,29 +219,40 @@ def plotter():
     previousmap = None
     RawVersionFitler = []
 
-    for version in p.versionfilter:
+
+    print("STEP 1")
+    for version in config['versionfilter']:
         RawVersionFitler.append(version)
         for d in range(10):
             RawVersionFitler.append(version+str(d))
-    p.wordfilter.extend(RawVersionFitler)
+    config['filter'].extend(RawVersionFitler)
+    print("STEP 2")
+    predata = timechunker(config['averagedays'])
+    print("STEP 2.2")
+    DailyAverageVal = IndexAverage(CSVDATA)
+    
+    print("STEP 2.5")
 
-    predata = timechunker(p.AverageDays)
-    DailyAverageVal = DailyAverage(timechunker(1))
     rawdata = SuffixRemover(predata)
     data = DuplicateMerger(rawdata)
     combineddata = dictmerger(data.values())
     mapcount = len(combineddata)
-    WhiteListedData = (weakfiller(combineddata,p.OnlyMapsContaining))
+    WhiteListedData = (weakfiller(combineddata,config["onlymaps"]))
     FilteredMaps = dictlimx(combineddata,WhiteListedData)
-    TopMaps = dictmax(FilteredMaps,p.MapsToShow)
+    TopMaps = dictmax(FilteredMaps,config["mapstoshow"])
+
+
+    print("STEP 3")
 
     YDICT = [dictlimx(datachunk,TopMaps) for datachunk in data.values()]
     YLIST = arrayrectifier([list(x.values()) for x in YDICT])
     Transpose_YLIST = np.array(np.transpose(YLIST))
     YMAX = [sum(x.values()) for x in data.values()]#max player count for each bar ascending order by date
     timerange = list(data.keys())#from earliest to latest dates in
-    XaxisDates2 = p.XaxisDates-1
+    XaxisDates2 = config['axisdates']-1
     X = []
+
+    print("STEP 4")
     if(XaxisDates2<1):
         XaxisDates2 = 1
 
@@ -233,22 +269,25 @@ def plotter():
         _Y = YCUR
         plotdraw(X,mapname)
 
-
+    print("STEP 5")
     othermapscreated = False
     if(type(previousmap) is not type(None) and len(TopMaps) != mapcount):
         force = True
         othermapscreated = True
         _Y = np.ones((len(previousmap)), dtype=int)-previousmap
         plotdraw(X,"Other Maps")
-        ax.set_title(f"Top {len(TopMaps)} Maps with keywords {p.OnlyMapsContaining} out of {mapcount} \nBar width as relative Player count")
+        ax.set_title(f"Top {len(TopMaps)} Maps with keywords {config['onlymaps']} out of {mapcount} \nBar width as relative Player count")
 
         ax.grid(True)
 
     elif(mapcount == 0):
-        ax.set_title(f"None maps found using keywords {p.OnlyMapsContaining}")
+        ax.set_title(f"None maps found using keywords {config['onlymaps']}")
 
     else:
-        ax.set_title(f"Top {len(TopMaps)} Maps with keywords {p.OnlyMapsContaining} out of {mapcount} \nBar width as relative Player count")
+        ax.set_title(f"Top {len(TopMaps)} Maps with keywords {config['onlymaps']} out of {mapcount} \nBar width as relative Player count")
+
+
+    print("STEP 6")
 
     legenddata = zip(Handles,Labels)
     legenddata2 = [{"Handle":i,"Label":x[0],"Percentage":x[1]} for i,x in legenddata]
@@ -261,21 +300,23 @@ def plotter():
         legendhandles.insert(0,legendhandles.pop(-1))
     
     
-    
+    print("STEP 7")
 
     box = ax.get_position()
     ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
-    ax.text(0.05, 0.95, f'Average Daily Player Count : {DailyAverageVal}',
+    if(DailyAverageVal > 5):    
+        ax.text(0.05, 0.95, f'Average Daily Player Count : {DailyAverageVal}',
     verticalalignment='bottom', horizontalalignment='left',
     transform=ax.transAxes,
     color='Black', fontsize=15)
     ax.grid(True)      
-    ax.legend(legendhandles,legendlabels,framealpha=p.LabelTransparency,loc='center left', bbox_to_anchor=(1, 0.5))
+    ax.legend(legendhandles,legendlabels,framealpha=config['labeltransparency'],loc='center left', bbox_to_anchor=(1, 0.5))
     dirname = os.path.dirname(os.path.realpath(__file__))
-    rawfilename = os.path.join(dirname,p.Filenamepng)
-
+    rawfilename = os.path.join(dirname,config["filenamepng"])
+    print("FINISHED")
     fig.savefig(rawfilename)
     plt.show()
 #init
 if __name__ == "__main__":
     plotter()
+    os.system("PAUSE")